cbe8f40e9e2fe4580c7fed5553b4fb40d7f18ccf
Kjell Konis <kjellk@gmail.com>
2023-10-02 11:04:51 -0700

    Added first testthat tests.

--- tests/testthat/test-dbi.table.R
+++ tests/testthat/test-dbi.table.R
@@ -1,38 +1,31 @@
-compare_with_data.table <- function(x, env = parent.frame(),
-                                    ignore.row.order = TRUE) {
-  x <- substitute(x)
-
-  stopifnot(is.dbi.table(env[[DBIT_name <- as.character(x[[2]])]]))
-
-  env_DT <- rlang::env_clone(env)
-  assign(DBIT_name, as.data.table(env[[DBIT_name]]), envir = env_DT)
-
-  DBIT <- as.data.table(eval(x, envir = env))
-  DT <- eval(x, envir = env_DT)
-
-  check <- data.frame(target_names = names(DT),
-                      target_types = vapply(DT, typeof, ""),
-                      current_names = names(DBIT),
-                      current_types = vapply(DBIT, typeof, ""))
-  check$check <- (check$target_names == check$current_names) &
-                   (check$target_types == check$current_types)
-  cat("\n")
-  print(x)
-  cat("\n")
-
-  print(check)
-  cat("\n")
-
-  print(eq <- all.equal(DT, DBIT, ignore.row.order = ignore.row.order))
-  cat("\n")
-
-  isTRUE(eq)
-}
+source("compare_with_data.table.R")
 
 
 
-test_that("", {
+test_that("dbi.table works", {
+  expect_no_error(conn <- ex_chinook())
+  expect_no_error(Album <- dbi.table(conn, DBI::Id(table = "Album")))
+  expect_true(is.dbi.table(Album))
+  expect_no_error(DBI::dbDisconnect(conn))
+})
+
+
+
+test_that("dbi.attach works", {
+  expect_no_error(dbi.attach(ex_chinook))
+  expect_type(e <- as.environment("RSQLite:Chinook_Sqlite"),
+              type =  "environment")
+  expect_vector(s <- ls(e), ptype = character())
+  expect_true(all(unlist(eapply(e, is.dbi.table))))
+  expect_silent(detach("RSQLite:Chinook_Sqlite"))
+})
+
+
+
+test_that("simple subset works", {
   expect_no_error(dbi.attach(ex_chinook))
   expect_s3_class(Album <- Album, "dbi.table")
+  expect_no_error(source("compare_with_data.table.R"))
   expect_true(compare_with_data.table(Album[AlbumId > 7 & AlbumId < 13]))
+  expect_silent(detach("RSQLite:Chinook_Sqlite"))
 })
