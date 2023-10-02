0929145461d8bc7fbec05a20f641c0941bca5925
Kjell Konis <kjellk@gmail.com>
2023-10-02 11:24:46 -0700

    Added comment describing test.

--- tests/testthat/compare_with_data.table.R
+++ tests/testthat/compare_with_data.table.R
@@ -1,5 +1,27 @@
+# Compare with reference result. Test the square bracket operator ([]) on a
+# single dbi.table.  Example: 'Album[AlbumId < 7 & ArtistId == 2, .(Title)]'
+#
+#  ## <Chinook_Sqlite> Album 
+#  ##             Title
+#  ## Balls to the Wall
+#  ## Restless and Wild
+#
+# This test evaluates the input two different ways:
+#
+#   1. (the typical dbi.table workflow) the input is translated to sql,
+#      evaluated by the connected database, and the results are downloaded to
+#      a data.table.
+#
+#   2. (reference) the entire database table is downloaded to a data.table then
+#      the square bracket call is computed locally.
+#
+# The goal of the dbi.table package is to match the reference answer.  Right now
+# matching up to row order is acceptable.
+
+
 compare_with_data.table <- function(x, env = parent.frame(),
-                                    ignore.row.order = TRUE) {
+                                    ignore.row.order = TRUE,
+                                    verbose = FALSE) {
   x <- substitute(x)
 
   stopifnot(is.dbi.table(env[[DBIT_name <- as.character(x[[2]])]]))
@@ -16,15 +38,20 @@ compare_with_data.table <- function(x, env = parent.frame(),
                       current_types = vapply(DBIT, typeof, ""))
   check$check <- (check$target_names == check$current_names) &
                    (check$target_types == check$current_types)
-  cat("\n")
-  print(x)
-  cat("\n")
 
-  print(check)
-  cat("\n")
+  eq <- all.equal(DT, DBIT, ignore.row.order = ignore.row.order)
 
-  print(eq <- all.equal(DT, DBIT, ignore.row.order = ignore.row.order))
-  cat("\n")
+  if (verbose) {
+    cat("\n")
+    print(x)
+    cat("\n")
+
+    print(check)
+    cat("\n")
+
+    print(eq)
+    cat("\n")
+  }
 
   isTRUE(eq)
 }
