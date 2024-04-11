c07637c30837ebb6abdda3d54ad793a35ae52c25
Kjell Konis <kjellk@gmail.com>
2024-04-10 21:54:00 -0700

    Copy database to temporary file so user interaction doesn't bork package example.

--- R/examples.R
+++ R/examples.R
@@ -3,25 +3,27 @@
 #' @description These functions return connections to example databases
 #'              included in the \pkg{dbi.table} package.
 #'
-#' @param type a scalar character string. Use \code{"DBI"} for a
-#'             \code{DBIConnection} or \code{path} for the database file path.
-#'
 #' @describeIn example_databases
 #'
 #' @export
-chinook.sqlite <- function(type = c("DBI", "path")) {
-  type <- match.arg(type)
-
+chinook.sqlite <- function() {
+  Chinook_Sqlite.sqlite <- "Chinook_Sqlite.sqlite"
   path <- system.file(package = "dbi.table")
-  path <- file.path(path, "example_files", "Chinook_Sqlite.sqlite")
+  path <- file.path(path, "example_files", Chinook_Sqlite.sqlite)
 
-  if (type == "path") {
-    return(path)
+  if (!dir.create(tmp_path <- tempfile("ex"))) {
+    stop("could not create directory ", tmp_path)
+  }
+
+  tmp_path <- file.path(tmp_path, Chinook_Sqlite.sqlite)
+
+  if (!file.copy(path, tmp_path, overwrite = FALSE, copy.mode = FALSE)) {
+    stop("could not copy 'Chinook_Sqlite.sqlite' to temporary directory")
   }
 
   if (requireNamespace("RSQLite")) {
-    DBI::dbConnect(RSQLite::SQLite(), path)
-  } else {
-    stop("the ", dQuote("RSQLite"), " package is required to use this example")
+    return(DBI::dbConnect(RSQLite::SQLite(), tmp_path))
   }
+
+  stop("the ", dQuote("RSQLite"), " package is required to use this example")
 }
