a94dd995675976491e2b632dd729dc987126f068
Kjell Konis <kjellk@gmail.com>
2024-07-20 09:28:01 -0700

    Added description of pkg scope.

--- R/reference_test.R
+++ R/reference_test.R
@@ -1,30 +1,46 @@
-#' \code{dbi.table} test vs. reference implementation
+#' Test \code{dbi.table} vs. Reference Implementation
 #'
-#' Evaluate an expression using \code{dbi.table}s and separately using
-#' \code{data.tables}s, then compare the results.
+#' Evaluate an expression including at least one \code{dbi.table} and compare
+#' the result with the \emph{Reference Implementation}. This function is
+#' primarily for testing and is potentially very slow for large tables.
 #'
-#' This function evaluates the input \code{x} in two different ways:
+#' @section Reference Implementation:
+#'   Suppose that \code{id1} identifies a table in a SQL database and that
+#'   \code{[i, j, by]} describes a subset/select/summarize operation using
+#'   \code{data.table} syntax. The \emph{Reference Implementation} for this
+#'   operation is:
 #'
-#'   1. (the typical dbi.table workflow) the input is translated to sql,
-#'      evaluated by the connected database, and the results are downloaded as
-#'      a data.table.
+#'   \code{setDT(dbReadTable(conn, id1))[i, j, by]}
 #'
-#'   2. (reference) the entire database table is downloaded to as data.table
-#'      then the expression is evaluated locally.
+#'   More generally, for an expression involving multiple SQL database objects
+#'   and using \code{data.table} syntax, the \emph{Reference Implementation}
+#'   would be to download each of these objects in their entirety, convert them
+#'   to \code{data.table}s, then evaluate the expression.
 #'
-#' The goal of the dbi.table package is to match the reference result. Matching
-#' up to row order is acceptable.
+#'   The goal of the \pkg{dbi.table} is to generate an SQL query that produces
+#'   the same results set as the Reference Implementation up to row ordering.
 #'
-#' @param expr an expression involving at least one \code{dbi.table}.
+#' @param expr
+#'   an expression involving at least one \code{dbi.table} and whose result can
+#'   be coerced into a \code{data.table}.
 #'
-#' @param envir an environment. Where to evaluate \code{expr}.
+#' @param envir
+#'   an environment. Where to evaluate \code{expr}.
 #'
-#' @param ignore.row.order a logical value. This argument is passed to
-#'                         \code{\link{all.equal}}.
+#' @param ignore.row.order
+#'   a logical value. This argument is passed to \code{\link{all.equal}}.
 #'
-#' @param verbose a logical value. When \code{TRUE}, the output from
-#'                \code{all.equal} is displaued in a message when
-#'                \code{all.equal} returns anything other than \code{TRUE}.
+#' @param verbose
+#'   a logical value. When \code{TRUE}, the output from \code{all.equal} is
+#'   displayed in a message when \code{all.equal} returns anything other than
+#'   \code{TRUE}.
+#'
+#' @examples
+#'   duck <- dbi.catalog(chinook.duckdb)
+#'   Album <- duck$main$Album
+#'   Artist <- duck$main$Artist
+#'
+#'   reference_test(merge(Album, Artist, by = "ArtistId"))
 #'
 #' @export
 reference_test <- function(expr, envir = parent.frame(),
@@ -40,7 +56,12 @@ reference_test <- function(expr, envir = parent.frame(),
 
   dbits <- lapply(dbits, as.data.table)
 
-  dbit_eval <- as.data.table(eval(expr, envir = envir))
+  dbit_eval <- eval(expr, envir = envir)
+  if (!(is.dbi.table(dbit_eval) || is.data.table(dbit_eval))) {
+    stop("'expr' must return a 'dbi.table' or a 'data.table'")
+  }
+
+  dbit_eval <- as.data.table(dbit_eval)
   dt_eval <- eval(expr, envir = dbits, enclos = envir)
 
   # merge sets key by default so unkey
