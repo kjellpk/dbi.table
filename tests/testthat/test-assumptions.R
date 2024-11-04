584d16d0192a5439699716bfa2becc51204b89f3
Kjell Konis <kjellk@gmail.com>
2024-11-03 19:18:06 -0800

    Removed RMariaDB assumption since fixed in source.

--- tests/testthat/test-assumptions.R
+++ tests/testthat/test-assumptions.R
@@ -9,16 +9,3 @@ test_that("dbplyr::translate_sql_ borks", {
     dbplyr::translate_sql_(list(quote(title %like% "bork")), con = conn)
   })
 })
-
-
-
-test_that("RMariaDB includes def when dbQuoteIdentifiering Ids", {
-  conn <- dbplyr::simulate_mariadb()
-  name <- c(catalog = "def", schema = "apples", table = "honeycrisp")
-  id <- DBI::Id(name)
-
-  expect_identical(
-    RMariaDB:::dbQuoteIdentifier_MariaDBConnection_Id(conn, id),
-    DBI::SQL(paste(DBI::dbQuoteIdentifier(conn, name), collapse = "."))
-  )
-})
