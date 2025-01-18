af3b1504f1573855322020c16bd41b63d2104721
Kjell Konis <kjellk@gmail.com>
2025-01-18 06:23:58 -0800

    Now can use interal names in on.

--- R/sql.join.R
+++ R/sql.join.R
@@ -42,7 +42,7 @@
 #' @export
 sql.join <- function(x, y, type = "inner", on = NULL,
                      prefixes = c("x.", "y.")) {
-  parent_frame <- parent.frame()
+  type <- match.arg(type, choices = names(JOIN_TYPES))
 
   if (!is.dbi.table(x)) {
     stop("'x' is not a 'dbi.table'")
@@ -51,20 +51,6 @@ sql.join <- function(x, y, type = "inner", on = NULL,
   if (!is.dbi.table(y)) {
     stop("'y' is not a 'dbi.table'")
   }
-
-  if (!can_join_as_x(x)) {
-    x <- as_cte(x)
-  }
-
-  x_ctes <- get_ctes(x)
-
-  if (!can_join_as_y(y)) {
-    y <- as_cte(y)
-  }
-
-  y_ctes <- get_ctes(y)
-
-  type <- match.arg(type, choices = names(JOIN_TYPES))
 
   on_sub <- substitute(on)
   if (inherits(on <- try(on, silent = TRUE), "try-error")) {
@@ -83,44 +69,69 @@ sql.join <- function(x, y, type = "inner", on = NULL,
     stop("'on' is not a 'call'")
   }
 
-  xref <- xref_in(x_names <- names(x), y_names <- names(y), prefixes)
-  names(x) <- paste0(prefixes[[1L]], x_names)
-  names(y) <- paste0(prefixes[[2L]], y_names)
-
   on_vars <- all.vars(on)
-
-  if (any(i <- !(on_vars %in% names(xref)))) {
+  if (any(i <- (on_vars %in% intersect(names(x), names(y))))) {
     stop("ambiguous use of '", v <- on_vars[i][1], "' in 'on'; use '",
          paste0(prefixes[[1L]], v), "' to refer to the '", v, "' in 'x' ",
          "and '", paste0(prefixes[[2L]], v), "' to refer to the '", v,
          "' in 'y'")
   }
 
+  prefixes <- as.character(prefixes)
+  if ((length(prefixes) != 2) || anyDuplicated(prefixes)) {
+    stop("'prefixes' is not a character vector containing 2 distinct values")
+  }
 
-  # 1. Join column definitions
+  if (nchar(prefixes[[2L]]) < 1L) {
+    stop("'prefixes[2]' must have at least 1 character")
+  }
 
-  on <- sub_lang(on, envir = xref, specials = NULL, enclos = parent_frame)
+  sql_join(x, y, type, on, prefixes, parent.frame())
+}
+
+
+sql_join <- function(x, y, type, on, prefixes, parent_frame) {
+  if (!can_join_as_x(x)) {
+    x <- as_cte(x)
+  }
+
+  x_ctes <- get_ctes(x)
+
+  if (!can_join_as_y(y)) {
+    y <- as_cte(y)
+  }
+
+  y_ctes <- get_ctes(y)
+
+
+  # 1. Join fields and column definitions
 
   x_fields <- get_fields(x)
+  x_fields$src <- prefixes[[1L]]
   y_fields <- get_fields(y)
-  y_sub <- paste0(session$key_base, nrow(x_fields) + seq_len(nrow(y_fields)))
-  names(y_sub) <- y_fields$internal_name
-  y_fields$internal_name <- unname(y_sub)
-  y_sub <- lapply(y_sub, as.name)
+  y_fields$src <- prefixes[[2L]]
 
-  xy <- c(c(x), sub_lang(y, envir = y_sub))
+  xy_fields <- rbind(x_fields, y_fields)
+  xy_fields$old <- xy_fields$internal_name
+  xy_fields$internal_name <- paste0(session$key_base, seq_len(nrow(xy_fields)))
 
-  x_where <- get_where(x)
-  if (length(y_where <- get_where(y))) {
-    y_where <- sub_lang(y_where, envir = y_sub)
-  }
-  xy_where <- c(x_where, y_where)
+  y_sub <- xy_fields[xy_fields$src == prefixes[[2L]], ]
+  y_sub <- names_list(y_sub$internal_name, y_sub$old)
+  new_y <- sub_lang(y, envir = y_sub)
 
-  x_order_by <- get_order_by(x)
-  if (length(y_order_by <- get_order_by(y))) {
-    y_order_by <- sub_lang(y_order_by, envir = y_sub, specials = NULL)
-  }
-  xy_order_by <- c(x_order_by, y_order_by)
+  xref <- xref_in(c(x), new_y, xy_fields, prefixes)
+  xy <- c(c(x), new_y)
+  on <- sub_lang(on, envir = xref, specials = NULL, enclos = parent_frame)
+
+  # join where
+
+  y_where <- sub_lang(get_where(y), envir = y_sub)
+  xy_where <- c(get_where(x), y_where)
+
+  # join order_by
+
+  y_order_by <- sub_lang(get_order_by(y), envir = y_sub, specials = NULL)
+  xy_order_by <- c(get_order_by(x), y_order_by)
 
   # 2. Join DBI connections (fail if not same connection)
 
@@ -132,23 +143,18 @@ sql.join <- function(x, y, type = "inner", on = NULL,
   y_data_source <- get_data_source(y)
 
   if (y_data_source$id_name %in% x_data_source$id_name) {
-    y_data_source$id_name <- unique_table_name()
-    y_fields$id_name <- y_data_source$id_name
+    new_id_name <- unique_table_name()
+    y_data_source$id_name <- new_id_name
+    xy_fields[xy_fields$src == prefixes[[2L]], ]$id_name <- new_id_name
   }
 
-
-  # 3. join fields
-
-  xy_fields <- rbind(x_fields, y_fields)
+  xy_fields <- xy_fields[, c("internal_name", "id_name", "field")]
 
 
   # 5. join data_source
 
   y_data_source$clause <- JOIN_TYPES[type]
-
-  if (!is.null(on)) {
-    y_data_source$on <- list(sub_lang(on, envir = xy, specials = NULL))
-  }
+  y_data_source$on <- list(on)
 
   xy_data_source <- rbind(x_data_source, y_data_source)
 
@@ -161,7 +167,7 @@ sql.join <- function(x, y, type = "inner", on = NULL,
 
   # 7. Set output names
 
-  dups <- intersect(x_names, y_names)
+  dups <- intersect(x_names <- names(x), y_names <- names(y))
   idx <- x_names %in% dups
   x_names[idx] <- paste0(prefixes[[1L]], x_names[idx])
   idx <- y_names %in% dups
@@ -200,35 +206,32 @@ can_join_as_y <- function(x) {
 
 
 
-xref_in <- function(x_names, y_names, prefixes) {
-  prefixes <- as.character(prefixes)
-  if ((length(prefixes) != 2) || anyDuplicated(prefixes)) {
-    stop("'prefixes' is not a character vector containing 2 distinct values",
-         call. = FALSE)
-  }
-
-  if (nchar(prefixes[[2L]]) < 1L) {
-    stop("'prefixes[2]' must have at least 1 character", call. = FALSE)
-  }
+xref_in <- function(x, y, fields, prefixes) {
+  x_names <- names(x)
+  y_names <- names(y)
 
   dups <- intersect(x_names, y_names)
 
-  input_names <- c(x_names, y_names)
-  prefixed_names <- c(paste0(prefixes[[1L]], x_names),
-                      paste0(prefixes[[2L]], y_names))
+  x_dups <- x[dups]
+  names(x_dups) <- paste_prefix(prefixes[[1L]], names(x_dups))
+  x_unique <- x[setdiff(x_names, dups)]
+  x_unique_pre <- x_unique
+  names(x_unique_pre) <- paste_prefix(prefixes[[1L]], names(x_unique_pre))
+  x <- c(x_dups, x_unique, x_unique_pre)
 
-  if (idx <- anyDuplicated(prefixed_names)) {
-    stop("prefixed names are not unique - first duplicate: ",
-         prefixed_names[idx], call. = FALSE)
-  }
+  y_dups <- y[dups]
+  names(y_dups) <- paste_prefix(prefixes[[2L]], names(y_dups))
+  y_unique <- y[setdiff(y_names, dups)]
+  y_unique_pre <- y_unique
+  names(y_unique_pre) <- paste_prefix(prefixes[[2L]], names(y_unique_pre))
+  y <- c(y_dups, y_unique, y_unique_pre)
 
-  xref <- prefixed_names
-  names(prefixed_names) <- prefixed_names
+  internal <- names_list(fields$internal_name, paste0(fields$src, fields$old))
 
-  names(xref) <- input_names
-  keep <- setdiff(setdiff(input_names, dups), prefixed_names)
-  xref <- xref[keep]
+  c(x, y, internal)
+}
 
-  xref <- c(prefixed_names, xref)
-  names_list(xref, names(xref))
+
+paste_prefix <- function(p, n) {
+  paste0(rep(p, length(n)), n)
 }
