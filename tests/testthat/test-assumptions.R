550fb6d4b958f03a99b1094492a9383ba777e04a
Kjell Konis <kjellk@gmail.com>
2025-01-26 07:09:13 -0800

    Added test that copy_vector actually copies vector.

--- tests/testthat/test-assumptions.R
+++ tests/testthat/test-assumptions.R
@@ -9,3 +9,13 @@ test_that("dbplyr::translate_sql_ borks", {
     dbplyr::translate_sql_(list(quote(title %like% "bork")), con = conn)
   })
 })
+
+
+test_that("copy_vector actually copies vector", {
+  v1 <- "vector"
+  a1 <- data.table::address(v1)
+  v2 <- copy_vector(v1)
+  a2 <- data.table::address(v2)
+  expect_identical(v1, v2)
+  expect_true(!identical(a1, a2))
+})
