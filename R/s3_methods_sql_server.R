146058d1b055b1e84756b0cf0e83c9957eec568e
Kjell Konis <kjellk@gmail.com>
2025-05-09 13:21:52 -0700

    Update SQL Server foreign_keys_ method.

--- R/s3_methods_sql_server.R
+++ R/s3_methods_sql_server.R
@@ -19,30 +19,30 @@ schemas_to_include_Microsoft_SQL_Server <- function(conn) {
 
 
 
-#' @rawNamespace S3method(foreign_keys_,"Microsoft SQL Server",foreign_keys_duckdb)
+#' @rawNamespace S3method(foreign_keys_,"Microsoft SQL Server",foreign_keys_Microsoft_SQL_Server)
 foreign_keys_Microsoft_SQL_Server <- function(catalog, id) {
-  r <- merge(catalog$information_schema$referential_constraints,
-             catalog$information_schema$key_column_usage,
-             by = c("constraint_catalog",
-                    "constraint_schema",
-                    "constraint_name"))
+  r <- merge(catalog$INFORMATION_SCHEMA$REFERENTIAL_CONSTRAINTS,
+             catalog$INFORMATION_SCHEMA$KEY_COLUMN_USAGE,
+             by = c("CONSTRAINT_CATALOG",
+                    "CONSTRAINT_SCHEMA",
+                    "CONSTRAINT_NAME"))
 
   names(r) <- paste0("fk_", names(r))
 
-  r <- merge(catalog$information_schema$key_column_usage, r,
-             by.x = c("constraint_catalog",
-                      "constraint_schema",
-                      "constraint_name",
-                      "ordinal_position"),
-             by.y = c("fk_unique_constraint_catalog",
-                      "fk_unique_constraint_schema",
-                      "fk_unique_constraint_name",
-                      "fk_ordinal_position"))
-
-  r <- r[, c("fk_constraint_name", "fk_table_catalog", "fk_table_schema",
-              "fk_table_name", "fk_column_name", "table_catalog",
-              "table_schema", "table_name", "column_name",
-              "fk_position_in_unique_constraint")]
+  r <- merge(catalog$INFORMATION_SCHEMA$KEY_COLUMN_USAGE, r,
+             by.x = c("CONSTRAINT_CATALOG",
+                      "CONSTRAINT_SCHEMA",
+                      "CONSTRAINT_NAME",
+                      "ORDINAL_POSITION"),
+             by.y = c("fk_UNIQUE_CONSTRAINT_CATALOG",
+                      "fk_UNIQUE_CONSTRAINT_SCHEMA",
+                      "fk_UNIQUE_CONSTRAINT_NAME",
+                      "fk_ORDINAL_POSITION"))
+  
+  r <- r[, c("fk_CONSTRAINT_NAME", "fk_TABLE_CATALOG", "fk_TABLE_SCHEMA",
+              "fk_TABLE_NAME", "fk_COLUMN_NAME", "TABLE_CATALOG",
+              "TABLE_SCHEMA", "TABLE_NAME", "COLUMN_NAME",
+              "ORDINAL_POSITION")]
 
   names(r) <- c("constraint_name", "fk_table_catalog", "fk_table_schema",
                 "fk_table_name", "fk_column_name", "pk_table_catalog",
