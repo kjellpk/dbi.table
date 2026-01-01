e8a896f142926370525ad8ad588a9f8a878e1471
Kjell Konis <kjellk@gmail.com>
2026-01-01 11:11:14 -0800

    Add regression test for issue 4.

---
+++ tests/testthat/test-regression.R
@@ -0,0 +1,15 @@
+for (n in names(memory_connections)) {
+  conn <- memory_connections[[n]]
+
+  test_that(paste0("issue 4: 'on = .(name = name2)' works in extract", " [", n, "]"), {
+    test <- data.table(name = c("Sam", "Tom", "Lee"),
+                       age = c(25, 35, 20))
+    test2 <- data.table(name2 = c("Sam", "Tom", "Lee"),
+                        job = c("electrician", "plumber", "scientist"))
+
+    table1 <- as.dbi.table(conn, test, type = "temporary")
+    table2 <- as.dbi.table(conn, test2, type = "temporary")
+
+    expect_no_error(table1[table2, on = .(name = name2)])
+  })
+}
