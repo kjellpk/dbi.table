b1c452d6dec3b7e024400fafc80f4d04f53c4bb9
Hadley Wickham <h.wickham@gmail.com>
2026-02-13 16:01:46 -0600

    Drop test
    I'm not sure what you're trying to test here as this is only dbplyr code. And it works with the dev version of dbplyr.

--- tests/testthat/test-assumptions.R
+++ tests/testthat/test-assumptions.R
@@ -1,16 +1,3 @@
-test_that("dbplyr::translate_sql_ borks", {
-  conn <- dbplyr::simulate_dbi()
-
-  expect_no_error({
-    dbplyr::translate_sql(title %like% "bork", con = conn)
-  })
-
-  expect_error({
-    dbplyr::translate_sql_(list(quote(title %like% "bork")), con = conn)
-  })
-})
-
-
 test_that("copy_vector actually copies vector", {
   v1 <- "vector"
   a1 <- data.table::address(v1)
