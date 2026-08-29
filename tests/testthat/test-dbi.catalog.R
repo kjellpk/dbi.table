bf512e90dbcf257ab2c300050729c113e270ac81
Kjell Konis <kjellk@gmail.com>
2026-08-29 16:11:15 -0600

    Use attribution on connection to store schema name.

--- tests/testthat/test-dbi.catalog.R
+++ tests/testthat/test-dbi.catalog.R
@@ -1,6 +1,10 @@
 for (n in names(chinook_connections)) {
   conn <- chinook_connections[[n]]
 
+  if (is.null(schema <- attr(conn, "schema", exact = TRUE))) {
+    schema <- "main"
+  }
+
   test_that(paste0("dbi.catalog works ", " [", n, "]"), {
     expect_no_error(chinook <- dbi.catalog(conn))
 
@@ -8,8 +12,8 @@ for (n in names(chinook_connections)) {
     expect_true(is_dbi_catalog(chinook))
 
     # is_dbi_schema is not exported
-    expect_true(is_dbi_schema(chinook$main))
+    expect_true(is_dbi_schema(chinook[[schema]]))
 
-    expect_true(is.dbi.table(chinook$main$Album))
+    expect_true(is.dbi.table(chinook[[schema]]$Album))
   })
 }
